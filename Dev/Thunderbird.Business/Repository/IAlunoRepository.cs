664502304a6bd561d57a025a6ea61b870381e1e3
Turah Xavier <turahxavierbr@gmail.com>
2013-07-27 18:21:48 -0300

    IRepository

---
+++ Dev/Thunderbird.Business/Repository/IAlunoRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thunderbird.Factory.Entity;
+
+namespace Thunderbird.Business.Repository
+{
+    public interface IAlunoRepository : IRepositoryBase<Aluno>
+    {
+    }
+}
+
+
---
+++ Dev/Thunderbird.Business/Repository/IRepositoryBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thunderbird.Business.Repository
+{
+    public interface IRepositoryBase<T>
+    {
+        T Salvar(T entity);
+        
+        T Alterar(T entity);
+
+        void Excluir(T entity);
+
+        T ObterPorId(int id);
+
+        IList<T> ObterTodos();
+
+    }
+
+}
